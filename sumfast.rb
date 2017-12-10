c86c7bc6cb071e0a620409e35998bb0b808de780
Alex <alexrudall@gmail.com>
2017-12-10 19:29:24 +0000

    Calculate answer!

--- sumfast.rb
+++ sumfast.rb
@@ -1,13 +1,21 @@
 require 'sinatra/base'
+require 'dentaku'
 
 class Sumfast < Sinatra::Base
 
   get '/' do
-    result = params[:sum]
+    if input = params[:sum]
+      result = calculate(input)
+    end
     erb :index, locals: { result: result }
   end
 
   post '/' do
+  end
+
+  def calculate(input)
+    calculator = Dentaku::Calculator.new
+    calculator.evaluate(input)
   end
 end
 
